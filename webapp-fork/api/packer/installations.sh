8b0c48fa5b9db373c9219f2f9deec28e224f6792
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-21 17:58:38 -0500

    dbname

--- webapp-fork/api/packer/installations.sh
+++ webapp-fork/api/packer/installations.sh
@@ -8,5 +8,5 @@ sudo npm install
 sudo touch .env
 echo "DB_HOST=localhost" | sudo tee -a .env
 echo "DB_PASSWORD=Jaishreekrishna@1998" | sudo tee -a .env
-echo "DB_DATABASENAME=assignment04" | sudo tee -a .env
+echo "DB_DATABASE=assignment04" | sudo tee -a .env
 echo "DB_USERNAME=root" | sudo tee -a .env
