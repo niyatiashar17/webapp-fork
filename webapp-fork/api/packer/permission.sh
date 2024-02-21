9d163736f8d3778f80b34a0244709f74ed4e3f1e
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-21 17:54:33 -0500

    changename

--- webapp-fork/api/packer/permission.sh
+++ webapp-fork/api/packer/permission.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo chmod +x /tmp/mysql.sh
-sudo chmod +x /tmp/nodejs.sh
-sudo chmod +x /tmp/webapp-installation.sh
+sudo chmod +x /tmp/install_mysql.sh
+sudo chmod +x /tmp/install_nodejs.sh
+sudo chmod +x /tmp/installations.sh
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 sudo chown -R csye6225:csye6225 /home/packer/
 sudo cp /home/packer/webapp/webapp.service /etc/systemd/system/
