0bd88b26e78f50470571b17a5179d5a224f0bdcf
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-21 21:29:23 -0500

    installation

--- webapp-fork/api/packer/install_mysql.sh
+++ webapp-fork/api/packer/install_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y mysql-server
+sudo dnf update -y
+sudo dnf install -y mysql-server
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
 echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD'; FLUSH PRIVILEGES; CREATE DATABASE assignment04;" | sudo mysql
