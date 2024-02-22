a3d84c4152c48f0c34ee5b6d284df6081814f897
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-22 15:32:39 -0500

    final changes

--- webapp-fork/api/packer/installations.sh
+++ webapp-fork/api/packer/installations.sh
@@ -6,10 +6,3 @@ sudo mkdir -p /opt/webapp/webapp-fork/api
 sudo unzip /tmp/webapp.zip -d /opt/webapp/webapp-fork/api/
 cd /opt/webapp/webapp-fork/api
 sudo npm install
-
-#create .env
-# echo "DB_PASSWORD=$DB_PASSWORD" | sudo tee -a .env
-# echo "DB_HOST=$DB_HOST" | sudo tee -a .env
-# echo "DB_DATABASE=$DB_DATABASE" | sudo tee -a .env
-# echo "DB_USERNAME=$DB_USERNAME" | sudo tee -a .env
-# echo "PORT=$PORT" | sudo tee -a .env
