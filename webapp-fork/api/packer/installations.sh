c2a757c34d61b8b74f1ec5729ab8944dc6d8fdec
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-22 14:31:48 -0500

    created .env

--- webapp-fork/api/packer/installations.sh
+++ webapp-fork/api/packer/installations.sh
@@ -6,3 +6,10 @@ sudo mkdir -p /opt/webapp/webapp-fork/api
 sudo unzip /tmp/webapp.zip -d /opt/webapp/webapp-fork/api/
 cd /opt/webapp/webapp-fork/api
 sudo npm install
+
+#create .env
+echo "DB_PASSWORD=root" | sudo tee .env
+echo "DB_HOST=localhost" | sudo tee .env
+echo "DB_DATABASE=Health" | sudo tee .env
+echo "DB_USERNAME=root" | sudo tee .env
+echo "PORT=8080" | sudo tee .env
--- webapp-fork/api/packer/service.sh
+++ webapp-fork/api/packer/service.sh
@@ -6,11 +6,6 @@ Description=Node.js Application
 After=network.target
 
 [Service]
-ENVIRONMENT=DB_PASSWORD='root'
-ENVIRONMENT=DB_HOST='localhost'
-ENVIRONMENT=DB_DATABASE='Health'
-ENVIRONMENT=DB_USERNAME='root'
-ENVIRONMENT=PORT='8080'
 Type=simple
 User=csye6225
 Group=csye6225
@@ -18,6 +13,8 @@ WorkingDirectory=/opt/webapp/
 ExecStart=/usr/bin/node /opt/webapp/webapp-fork/api/index.js
 Restart=on-failure
 RestartSec=3
+StandardOutput=append:/var/log/webapp_output.log
+StandardError=append:/var/log/webapp_error.log
 
 [Install]
 WantedBy=multi-user.target
