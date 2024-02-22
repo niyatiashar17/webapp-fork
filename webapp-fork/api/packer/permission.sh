c41f149d8313b400bf5837fc8c6ec0eb4ebe9d13
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-21 23:35:19 -0500

    label error

--- webapp-fork/api/packer/permission.sh
+++ webapp-fork/api/packer/permission.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# sudo chmod +x /tmp/install_mysql.sh
-# sudo chmod +x /tmp/install_nodejs.sh
-# sudo chmod +x /tmp/installations.sh
 sudo groupadd -r csye6225
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225 
 sudo chown -R csye6225:csye6225 /opt/webapp/
