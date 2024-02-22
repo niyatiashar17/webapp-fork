15034530333611b862d4565c60f17f27c8689900
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-22 00:40:39 -0500

    passowrd change

--- webapp-fork/api/packer/install_mysql.sh
+++ webapp-fork/api/packer/install_mysql.sh
@@ -3,4 +3,4 @@
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
 sudo systemctl enable mysqld
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD'; FLUSH PRIVILEGES; CREATE DATABASE Health;" | sudo mysql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES; CREATE DATABASE Health;" | sudo mysql
