84a6533a81e68496f817c41f8ac92ed30803e534
Niyati Ashar <niyatiashar17@gmail.com>
2024-02-22 13:58:08 -0500

    made directory

--- webapp-fork/api/packer/installations.sh
+++ webapp-fork/api/packer/installations.sh
@@ -2,6 +2,7 @@
 
 cd /
 sudo dnf install unzip -y
+sudo mkdir -p /opt/webapp/webapp-fork/api
 sudo unzip /tmp/webapp.zip -d /opt/webapp/webapp-fork/api/
 cd /opt/webapp/webapp-fork/api
 sudo npm install
