a7e1fa055e1eeeb7f8e350b5a466549755a4ed07
niyatiashar17 <ashar.n@northeatsern.edu>
2024-03-19 13:06:36 -0400

    workflow updates

--- pkr/installations.sh
+++ pkr/installations.sh
@@ -4,6 +4,5 @@ cd /
 sudo dnf install unzip -y
 sudo mkdir -p /opt/webapp/
 sudo unzip /tmp/webapp.zip -d /opt/webapp/
-sudo mkdir -p /var/log/webapp/
 cd /opt/webapp/
 sudo npm install
--- pkr/permission.sh
+++ pkr/permission.sh
@@ -2,9 +2,10 @@
 
 sudo groupadd csye6225
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225
-sudo chown -R csye6225:csye6225 /opt/webapp/
+sudo mkdir -p /var/log/webapp/
+sudo chown -R csye6225:csye6225 /opt/webapp/ /var/log/webapp/
 sudo chown -R csye6225:csye6225 /etc/systemd/system/webapp.service
-sudo chmod 700 /opt/webapp/
+sudo chmod 700 /opt/webapp/ /var/log/webapp/
 sudo chmod 700 /etc/systemd/system/webapp.service
 echo "csye6225 ALL=(ALL:ALL) NOPASSWD: /bin/systemctl" | sudo EDITOR='tee -a' visudo
 sudo systemctl daemon-reload
